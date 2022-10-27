eec2fd931e3c5ed4e065f5aefccad6326662d295
Shireen Koul <67394758+koulshireen@users.noreply.github.com>
2022-10-27 18:59:22 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
