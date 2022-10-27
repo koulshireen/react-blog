3548672636c062c09bc132fa8d56399b3e195336
Shireen Koul <shireen.koul@shopify.com>
2022-10-27 23:15:59 +0000

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+  const handleClick = (event) => {
+    console.log("Hello, Bloggers!!", event.target);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}> Click me </button>
     </div>
   );
 };
