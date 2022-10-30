1b654cfdb0d7f60a80381d474ed529bd67fe3d0b
Shireen Koul <shireen.koul@shopify.com>
2022-10-30 20:40:55 +0000

    Fetch data from local JSON Server and remove delete button.

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum", author: "Shireen", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum", author: "Koul", id: 2 },
-    { title: "The summit!", body: "lorem ipsum", author: "Shireen", id: 3 },
-  ]);
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs!!" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title="All blogs!!" />}
     </div>
   );
 };
